7d044e84e2ae7ecd35b550cecb44f2a63e21ced9
joemou <zxc123asd66@gmail.com>
2026-01-22 17:38:46 +0800

    feat: Add default AppConfig

---
+++ setting/AppConfig.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+namespace AppConfig {
+    static const std::string TOPOLOGY_FILE = "../StaticNetworkTopology_ipAlias4_9Switches_all_1g_cable.json";
+    static const std::string SIM_SERVER_URL = "http://10.10.10.5:9000/submit";
+    static const std::string GW_IP = "10.10.10.1";
+    static const std::string RYU_IP_AND_PORT = "10.10.10.2:6633";
+    static const std::string TOPOLOGY_FILE_MININET = "../StaticNetworkTopology_ipAlias4_9Switches_all_1g_cable.json";
+}
